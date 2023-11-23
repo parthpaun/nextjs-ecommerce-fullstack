08694922f0f6d65f31256a6c45ed19648f321573
ppaun <ppaun@ibaset.com>
2023-11-23 18:27:24 +0530

    remove tthe console

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,4 @@
 export const request = async (url, options) => {
-  console.log("`${process.env.HOST}${url}`", process.env.NEXT_PUBLIC_HOST);
   const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}${url}`, { ...options });
   return res;
 };
